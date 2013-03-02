98f6bcfd9f4363de8e31e8a669d60f192e9c7e8c
Paulo Molos <molos_paulo@yahoo.com>
2013-03-02 22:57:25 +0800

    Update project.cpp

--- project_molos_and_sadiwa/src/project.cpp
+++ project_molos_and_sadiwa/src/project.cpp
@@ -518,9 +518,103 @@ void project::power_digit_sum()
     std::cout << std::accumulate( digits.begin(), digits.end(), 0 ) << '\n' ;
 }
 
+unsigned int value = 11;
+
+short small(short x);
+void two(short third);
+
 void project::number_letter_counts()
 {
-    cout<<"Unable to meet program requirements.."<<endl;
+for(short count = 0;count<10;count++)
+    {
+        two(count);
+    }
+   cout<<value<<endl;
+    cin.get();
+
+}
+
+void two(short third)
+{
+
+    string temp;
+    if(third>0)
+    {
+         third = (small(third) + 10);
+    }
+    for(short i = 0;i<20;i++)
+    {
+        value += (small(i) + third);
+    }
+    for(short i = 20;i<60;i++)
+    {
+        stringstream ss;
+         ss<<i;
+         temp = ss.str();
+         value += ((small(temp[1]-'0') + 6) + third);
+    }
+    for(short i = 40;i<70;i++)
+    {
+        stringstream ss;
+         ss<<i;
+         temp = ss.str();
+         value += ((small(temp[1]-'0') + 5) + third);
+    }
+    for(short i = 70;i<80;i++)
+    {
+         stringstream ss;
+         ss<<i;
+         temp = ss.str();
+         value += ((small(temp[1]-'0') + 7) + third);
+    }
+}
+
+short small(short x)
+{
+              switch(x)
+             {
+                case 0:
+                     return 0;
+                case 1:
+                     return 3;
+                case 2:
+                     return 3;
+                case 3:
+                     return 5;
+                case 4:
+                     return 4;
+                case 5:
+                     return 4;
+                case 6:
+                     return 3;
+                case 7:
+                     return 5;
+                case 8:
+                     return 5;
+                case 9:
+                     return 4;
+                case 10:
+                     return 3;
+                case 11:
+                     return 6;
+                case 12:
+                     return 6;
+                case 13:
+                     return 8;
+                case 14:
+                     return 8;
+                case 15:
+                     return 7;
+                case 16:
+                     return 7;
+                case 17:
+                     return 9;
+                case 18:
+                     return 8;
+                case 19:
+                     return 8;
+          }
+
 }
 
 void project::max_path_sum_one()
